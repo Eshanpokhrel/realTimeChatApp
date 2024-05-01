f3924aa9ff14993c9ab64e688c21f0417d3e5f1f
Eshanpokhrel <eshanpokhrel12@gmail.com>
2024-05-01 23:17:03 +0545

    successfully tested signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,54 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js"
+
+export const signup = async (req, res) => {
     try {
         const { firstName, lastName, username, gender, password, confirmPassword } = req.body
+
+        if(password !== confirmPassword){
+            return res.status(400).json({
+                message: "passwords do not match"
+            })
+        }
+
+        const user = await User.findOne({ username })
+
+        if(user){
+            return res.status(400).json({
+                message: "username already exists"
+            })
+        }
+
+        //Hashing password
+        // const salt = await bcrypt.genSalt(10)
+        // const hashedPassword = await bcrypt.hash(password, salt)
+
+        //profile avatar
+        // const male = ``
+        // const female = ``
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            username,
+            gender,
+            password,
+            // avatar: gender === 'male' ? male : female
+        })
+
+        await newUser.save()
+
+        res.status(201).json({
+            _id: newUser._id,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            username: newUser.username,
+            // avatar: newUser.avatar,
+            message: "user created successfully"
+        })
+
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({message: "Error creating user"})
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,18 @@ import authRoute from "./routes/auth.routes.js"
 import mongoDB from './db/mongoDB.js'
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
-
+app.use(express.json()) //parse the incoming req with json payload from req.body
 app.use("/api/auth", authRoute)
+
+
+// app.get('/', (req, res) => {
+//     res.send("hello world")
+// })
+
 
 app.listen(PORT, () => {
     mongoDB()
