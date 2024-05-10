e8ff89300be0a1834a693e71840052f6c272fd22
Eshanpokhrel <109594115+Eshanpokhrel@users.noreply.github.com>
2024-05-10 23:44:05 +0545

    UI changes

--- frontend/src/components/messages/Header.jsx
+++ frontend/src/components/messages/Header.jsx
@@ -10,33 +10,29 @@ import useConversation from "../../zustand/useConversation";
 const Header = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
-  // useEffect(() => {
-  //   return () => setSelectedConversation(null)    //component unmounting(clears the message window)
-  // },[setSelectedConversation])
-
   return (
-    <>
-      <div className="lg:hidden">
-        <FontAwesomeIcon icon={faArrowLeft} onClick={() => setSelectedConversation(null)}/>
+    <div className="h-[15%] w-full p-3 flex justify-between border-2 border-slate-500">
+      <div className="flex items-center gap-4">
+        <FontAwesomeIcon
+          className="h-5 lg:hidden"
+          icon={faArrowLeft}
+          onClick={() => setSelectedConversation(null)}
+        />
+        <img
+          className="w-[50px] h-[50px] rounded-full"
+          src={selectedConversation.avatar}
+          alt="avatar"
+        />
+        <span className="font-black text-xl">
+          {selectedConversation.fullName}
+        </span>
       </div>
-      <div className="h-[15%] w-full p-3 flex justify-between border-2 border-slate-500">
-        <div className="flex items-center gap-4">
-          <img
-            className="w-[50px] h-[50px] rounded-full"
-            src={selectedConversation.avatar}
-            alt="avatar"
-          />
-          <span className="font-black text-xl">
-            {selectedConversation.fullName}
-          </span>
-        </div>
-        <div className="flex-center gap-6">
-          <FontAwesomeIcon icon={faPhone} className="hover:text-white" />
-          <FontAwesomeIcon icon={faVideo} className="hover:text-white" />
-          <FontAwesomeIcon icon={faListDots} className="hover:text-white" />
-        </div>
+      <div className="flex-center gap-6">
+        <FontAwesomeIcon icon={faPhone} className="hover:text-white" />
+        <FontAwesomeIcon icon={faVideo} className="hover:text-white" />
+        <FontAwesomeIcon icon={faListDots} className="hover:text-white" />
       </div>
-    </>
+    </div>
   );
 };
 
